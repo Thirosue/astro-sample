ef0d6b9d778eb3a91cc223c799ce2a837215f26f
takeshi.hirosue <mirrored1976@gmail.com>
2023-03-13 16:11:19 +0900

    feat: cloudflare build settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
-  integrations: [tailwind(), react()],
+  adapter: cloudflare(),
+  integrations: [tailwind(), react()]
 });
